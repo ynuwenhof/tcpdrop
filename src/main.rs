677e13672e5fb9ae2bf21f671446593506eca1a5
Yannic Nuwenhof <ynuwenhof@pm.me>
2023-10-10 12:07:31 +0200

    Add basic command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use clap::Parser;
+use std::path::PathBuf;
+
 mod tcp;
 
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, env = "TCPDROP_PRETTY")]
+    pretty: bool,
+    #[arg(long, env = "TCPDROP_NO_V4")]
+    no_v4: bool,
+    #[arg(long, env = "TCPDROP_NO_V6")]
+    no_v6: bool,
+    #[arg(short, long, env = "TCPDROP_OUTPUT", value_name = "FILE")]
+    output: Option<PathBuf>,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     println!("Hello, world!");
 }
